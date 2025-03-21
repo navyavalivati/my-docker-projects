376752a27d62cd791580979ac228ba0f110c89d3
Navya Valivati <valevati.navya@gmail.com>
2025-03-20 22:17:49 -0500

    changes

--- beginner/hello-world/run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo "-------Running hello-world container--------"
-docker run hello-world
-echo "--------Listing all containers:----------"
-docker ps -a
-echo "Note: Use 'docker logs <container-id>' to see output, and 'docker rm <container-id>' to clean up."
